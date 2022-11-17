4d4b045ce2f17642e9f600f965894d1914813dd4
sambilokonga <111077805+sambilokonga@users.noreply.github.com>
2022-11-17 10:01:54 +0300

    Update aux_error1.c

--- aux_error1.c
+++ aux_error1.c
@@ -9,7 +9,28 @@
  * @ver_str: counter lines
  * Return: error message
  */
+char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
+{
+        char *illegal_flag;
 
+        _strcpy(error, datash->av[0]);
+        _strcat(error, ": ");
+        _strcat(error, ver_str);
+        _strcat(error, ": ");
+        _strcat(error, datash->args[0]);
+        _strcat(error, msg);
+        if (datash->args[1][0] == '-')
+        {
+                illegal_flag = malloc(3);
+                illegal_flag[0] = '-';
+                illegal_flag[1] = datash->args[1][1];
+                illegal_flag[2] = '\0';
+                _strcat(error, illegal_flag);
+                free(illegal_flag);
+        }
+        else
+        {
+                _strcat(error, datash->args[1]);
 	}
 
 	_strcat(error, "\n");
